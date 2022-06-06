aa052951e6546bb50e2f397d436cf93c122d83da
luisj2795 <luiscedenor95@gmail.com>
2022-06-06 15:39:42 -0400

    Se agreagaron Endpoints de Paises

---
+++ HotelListingAPI/Controllers/PaisController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListingAPI.Data;
+
+namespace HotelListingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+        private readonly HotelListingDbContext _context;
+
+        public PaisController(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pais
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pais>>> GetPaises()
+        {
+            var paises = await _context.Paises.ToListAsync();
+            return Ok(paises);
+        }
+
+        // GET: api/Pais/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pais>> GetPais(int id)
+        {
+            var pais = await _context.Paises.FindAsync(id);
+
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return pais;
+        }
+
+        // PUT: api/Pais/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPais(int id, Pais pais)
+        {
+            if (id != pais.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pais).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pais
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pais>> PostPais(Pais pais)
+        {
+            _context.Paises.Add(pais);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPais", new { id = pais.Id }, pais);
+        }
+
+        // DELETE: api/Pais/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePais(int id)
+        {
+            var pais = await _context.Paises.FindAsync(id);
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            _context.Paises.Remove(pais);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaisExists(int id)
+        {
+            return _context.Paises.Any(e => e.Id == id);
+        }
+    }
+}
